0e123526ea20c6e41afd00e89ba75eb39be9ead5
d-f <118086192+d-f@users.noreply.github.com>
2023-08-04 14:12:25 -0600

    Update pytorch_ViT_finetune.py

--- pytorch_ViT_finetune.py
+++ pytorch_ViT_finetune.py
@@ -1,40 +1,50 @@
-import torchvision
 import torch
 import torch.optim as optim
-import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
-import pandas as pd
-from torch.utils.data import Dataset
-from skimage import io
-# from torchsummary import summary
 from tqdm import tqdm
 import argparse
-from vit_pytorch import ViT, Dino
+from vit_pytorch import ViT
 from pathlib import Path
 import numpy as np
 import os
 import csv
-import json
 from sklearn.preprocessing import OneHotEncoder
+from typing import List
 from utils.data_utils import *
 from utils.model_utils import *
 
 
-def train(args, train_loader, model, optimizer, device, val_loader, criterion):
+def train(
+        train_loader: DataLoader, 
+        model: ViT, 
+        optimizer: optim, 
+        device: torch.device, 
+        val_loader: DataLoader, 
+        criterion: torch.nn.CrossEntropyLoss,
+        project_directory: Path,
+        model_save_name: str,
+        num_epochs: int,
+        patience: int
+        ) -> None:
     best_loss = np.inf
     patience_counter = 0
     # if loss log file exists, remove to not include previous training runs
-    if os.path.exists(Path(args.project_directory).joinpath('results_and_models').joinpath(
-            f'{args.model_save_name[:-8]}_finetune_loss_values.csv')):
-        os.remove(Path(args.project_directory).joinpath('results_and_models').joinpath(
-            f'{args.model_save_name[:-8]}_finetune_loss_values.csv'))
-    for epoch in range(args.num_epochs):
-        if patience_counter == args.patience:
+    if os.path.exists(Path(project_directory).joinpath('results').joinpath(
+            f'{model_save_name[:-8]}_finetune_loss_values.csv')):
+        os.remove(Path(project_directory).joinpath('results').joinpath(
+            f'{model_save_name[:-8]}_finetune_loss_values.csv'))
+    for epoch in range(num_epochs):
+        if patience_counter == patience:
             break
         losses = []
+        val_losses = []
         checkpoint = {'state_dict': model.state_dict(), 'optimizer': optimizer.state_dict()}
         model.train()
+        i=0
         for data, labels in tqdm(train_loader):
+            if i == 3:
+                break
+            i += 1
             data    = data.to(device=device) 
             labels = labels.to(device=device)
             scores  = model(data)
@@ -44,13 +54,20 @@ def train(args, train_loader, model, optimizer, device, val_loader, criterion):
             optimizer.step()
             with torch.no_grad():
                 losses.append(loss.item())
-        with torch.no_grad():        
+        with torch.no_grad(): 
+            val_losses = validate_model(
+                model=model,
+                val_loader=val_loader, 
+                device=device,
+                criterion=criterion,
+                val_losses=val_losses, 
+            )       
             total_loss = sum(losses) / len(losses)
             if total_loss < best_loss:
                 best_loss = total_loss
                 save_checkpoint(
                 state=checkpoint,
-                filepath=Path(args.project_directory).joinpath('results_and_models').joinpath(args.model_save_name)
+                filepath=Path(project_directory).joinpath('results').joinpath(model_save_name)
                 )
                 patience_counter = 0
             else:
@@ -58,12 +75,12 @@ def train(args, train_loader, model, optimizer, device, val_loader, criterion):
 
             print('epoch', epoch, 'loss: ', total_loss, 'patience counter', patience_counter)
 
-            if os.path.exists(Path(args.project_directory).joinpath('results_and_models').joinpath(f'{args.model_save_name[:-8]}_dino_loss_values.csv')) == False:
-                with open(Path(args.project_directory).joinpath('results_and_models').joinpath(f'{args.model_save_name[:-8]}_dino_loss_values.csv'), mode="w", newline="") as data:
+            if os.path.exists(Path(project_directory).joinpath('results').joinpath(f'{model_save_name[:-8]}_dino_loss_values.csv')) == False:
+                with open(Path(project_directory).joinpath('results').joinpath(f'{model_save_name[:-8]}_dino_loss_values.csv'), mode="w", newline="") as data:
                     csv_writer = csv.writer(data)
                     csv_writer.writerow((epoch, total_loss))
             else:
-                with open(Path(args.project_directory).joinpath('results_and_models').joinpath(f'{args.model_save_name[:-8]}_dino_loss_values.csv'), mode="a", newline="") as data:
+                with open(Path(project_directory).joinpath('results').joinpath(f'{model_save_name[:-8]}_dino_loss_values.csv'), mode="a", newline="") as data:
                     csv_writer = csv.writer(data)
                     csv_writer.writerow((epoch, total_loss))
 
@@ -72,8 +89,7 @@ def test_best_model(
     weight_path: str, 
     num_classes: int, 
     loader: torch.utils.data.DataLoader, 
-    model, 
-    model_save_dir: Path, 
+    model: ViT, 
     device: torch.device
     ) -> tuple([list, list, list, list]):
     """
@@ -95,13 +111,16 @@ def test_best_model(
         onehot_encoder = onehot_encoder.fit(
             np.array([x for x in range(num_classes)]).reshape(-1, 1)
         )
-
+        i=0
         for imgs, labels in tqdm(loader, desc="Test"):
+            if i == 3:
+                break
+            i += 1
             imgs = imgs.to(device=device)
             labels = labels.to(device=device)
             scores = model(imgs)
             label_list += [int(x) for x in labels]
-            prediction_list += [int(np.argmax(x)) for x in scores]
+            prediction_list += [int(np.argmax(x.cpu())) for x in scores]
             pred_prob_list += [tuple(x.cpu().numpy().astype(np.float64)) for x in scores]
             # labels is a list of tensors, so they need to be placed onto the cpu, converted to numpy arrays, 
             # reshaped with .reshape(-1, 1) to go from [1, 0] to [[1, 0]], transformed with onehot encoder, 
@@ -114,9 +133,9 @@ def test_best_model(
 def create_argparser():
     parser = argparse.ArgumentParser()
     # directory where all relevant folders are located
-    parser.add_argument("-project_directory", type=str, default="C:\\personal_ML\\DINOVIT_PCAM\\")
+    parser.add_argument("-project_directory", type=Path)
     # number of epochs to train for
-    parser.add_argument("-num_epochs", type=int, default=300)
+    parser.add_argument("-num_epochs", type=int, default=64)
     # number of classes to predict between
     parser.add_argument("-num_classes", type=int, default=2)
     # proportion to weight parameter update by
@@ -130,42 +149,43 @@ def create_argparser():
     # channels first
     parser.add_argument("-img_shape", default=(3, 96, 96), type=tuple, nargs="+")
     # size of image patch, 8, 16 and 32 are good values
-    parser.add_argument("-patch_size", type=int, default=16) # 8
+    parser.add_argument("-patch_size", type=int, default=8)
     # last dimension of output tensor after linear transformation
-    parser.add_argument("-dim", type=int, default=1024)  # 1024
+    parser.add_argument("-dim", type=int, default=1024)
     # number of transformer blocks
-    parser.add_argument("-depth", type=int, default=6)  # 6
+    parser.add_argument("-depth", type=int, default=6)
     # number of heads in multi-head attention layer
-    parser.add_argument("-heads", type=int, default=8)  # 8
+    parser.add_argument("-heads", type=int, default=8)
     # dimension of multilayer perceptron layer
-    parser.add_argument("-mlp_dim", type=int, default=2048)  # 2048
+    parser.add_argument("-mlp_dim", type=int, default=2048)
     parser.add_argument("-weight_path", type=Path)
     parser.add_argument("-param_str", choices=["just_classifier", "all"], default="just_classifier") # decides which layers to train
     return parser.parse_args()
 
 
 def validate_model(
-    model, 
-    val_loader, 
-    device, 
-    criterion, 
-    val_losses, 
-    num_correct_val
-    ):
+    model: ViT, 
+    val_loader: DataLoader, 
+    device: torch.device, 
+    criterion: torch.nn.CrossEntropyLoss, 
+    val_losses: List
+    ) -> List:
     """
     evaluates model performance on the validation set
     """
     with torch.no_grad():
         model.eval()  # turn dropout and batch norm off
+        i=0
         for val_data, val_targets in tqdm(val_loader, desc='Validation'):
+            if i == 3:
+                break
+            i += 1
             val_data = val_data.to(device=device)
             val_targets = val_targets.to(device=device)
             val_scores = model(val_data)
-            _, val_predictions = val_scores.max(1)
-            num_correct_val += (val_predictions == val_targets).sum().detach()
             val_loss = criterion(val_scores, val_targets)
             val_losses.append(val_loss.item())
-        return val_losses, num_correct_val
+        return val_losses
 
 
 def main():
@@ -192,17 +212,44 @@ def main():
         test_dataset=test_dataset,
         batch_size=args.batch_size
     )
-    train(args=args, train_loader=train_dataloader, model=model, optimizer=optimizer, device=device, val_loader=val_dataloader, criterion=criterion)
-    save_ft_results(args=args, optimizer=optimizer)
-    test_best_model(
-        weight_path=args.weight_path,
+    train(
+        train_loader=train_dataloader, 
+        model=model, 
+        optimizer=optimizer, 
+        device=device, 
+        val_loader=val_dataloader, 
+        criterion=criterion,
+        project_directory=args.project_directory,
+        model_save_name=args.model_save_name,
+        num_epochs=args.num_epochs,
+        patience=args.patience
+        )
+    save_ft_results(        
+        optimizer=optimizer,
+        batch_size=args.batch_size,
+        model_save_name=args.model_save_name, 
+        patience=args.patience,
+        patch_size=args.patch_size,
+        dim=args.dim,
+        depth=args.depth,
+        heads=args.heads,
+        mlp_dim=args.mlp_dim,
+        project_directory=args.project_directory
+        )
+    label_list, prediction_list, label_prob_list, pred_prob_list = test_best_model(
+        weight_path=args.project_directory.joinpath("models").joinpath(args.model_save_name),
         num_classes=args.num_classes, 
         loader=test_dataloader, 
         model=model, 
-        model_save_dir=args.project_directory.joinpath("models"),
         device=device
     )
-
+    save_test_results(
+        label_list=label_list,
+        prediction_list=prediction_list,
+        label_prob_list=label_prob_list,
+        pred_prob_list=pred_prob_list,
+        result_fp=args.project_directory.joinpath("results").joinpath(f"{args.model_save_name.partition('.pth.tar')[0]}_test_results.json")
+    )
 
 if __name__ == '__main__':
     main()
