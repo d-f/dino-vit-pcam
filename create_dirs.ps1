9e7238c115b31cf2cf3f7db79367bdaf159d2728
d-f <118086192+d-f@users.noreply.github.com>
2023-08-04 15:10:53 -0600

    Add files via upload

---
+++ create_dirs.ps1
@@ -0,0 +1,4 @@
+$proj_dir = $args[0]
+
+New-Item -Path "$proj_dir/results" -ItemType Directory
+New-Item -Path "$proj_dir/models" -ItemType Directory
---
+++ create_dirs.sh
@@ -0,0 +1,5 @@
+echo "Enter project directory"
+read proj_dir
+
+mkdir "$proj_dir/results"
+mkdir "$proj_dir/models"
